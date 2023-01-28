9bd511548f7c107553c1c01fd9e8b4fd189ffcb7
Christopher Lambert <christopherlambert106@gmail.com>
2023-01-28 08:32:22 -0800

    Added the key loading

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+import openai
+try:
+    from keys import *
+except:
+    API_KEY = input("OpenAI Key: ")
+
+class ChatGPTClone():
+    def __init__(self):
+        pass
+
+if __name__ == "__main__":
+    return
